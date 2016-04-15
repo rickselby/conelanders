f315aee3de72bcc3aba8cca6c1527983df890705
rickselby <rick@selby-family.co.uk>
2016-04-15 12:48:35 +0100

    Better regex
    Improve the regex for matching times in the table sorter

--- resources/views/tablesorter.blade.php
+++ resources/views/tablesorter.blade.php
@@ -1,8 +1,8 @@
 <script type="text/javascript">
     $.tablesorter.addParser({
-        id: "dayHoursMinutes",
+        id: "HoursMinutesSecondsMilliseconds",
         is: function (s) {
-            return /^[\d:.]+$/.test(s);
+            return /^((\d{1,2}:)?\d)?\d:\d{2}.\d{3}$/.test(s);
         }, format: function (s) {
             return s.replace(/[:.]/g, '');
         }, type: "numeric"
