007a33b8c4f4ccab813b4e41794d4acb8feb3af8
rickselby <rick@selby-family.co.uk>
2016-06-03 15:53:53 +0100

    Update race-not-complete.blade.php

--- resources/views/assetto-corsa/race-not-complete.blade.php
+++ resources/views/assetto-corsa/race-not-complete.blade.php
@@ -1,6 +1,6 @@
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Race not yet complete</h3>
+        <h3 class="panel-title">Race results not available</h3>
     </div>
     <div class="panel-body">
         This race may not have run, or the results may not have been released yet.
