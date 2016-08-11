c68d8164952152ec8899c08d098a0b7ec3cd6f60
rickselby <rick@selby-family.co.uk>
2016-08-11 12:07:09 +0100

    Update outline.blade.php

--- resources/views/outline.blade.php
+++ resources/views/outline.blade.php
@@ -15,7 +15,7 @@
         <link href="{{ asset('bower/tablesorter/dist/css/theme.bootstrap.min.css') }}" rel="stylesheet">
         <link href="{{ asset('bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" />
         <link href="{{ asset('bower/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
         @stack('stylesheets')
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
