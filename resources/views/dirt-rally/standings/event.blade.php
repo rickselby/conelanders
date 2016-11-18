c70b85d97d786d97c8dfb2dbdb402046d27d0e7a
Rick Selby <rick@selby-family.co.uk>
2016-11-18 12:05:25 +0000

    Show stages...

--- resources/views/dirt-rally/standings/event.blade.php
+++ resources/views/dirt-rally/standings/event.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-    @include('dirt-rally.event.summary')
+    <div class="panel panel-default">
+        <ul class="list-group">
+            @foreach($event->stages AS $stage)
+                <li class="list-group-item">
+                    <a href="{{ route('dirt-rally.standings.stage', [$event->season->championship, $event->season, $event, $stage]) }}" class="tablesorter-noSort">
+                        <strong>{{ $stage->ss }}:</strong>
+                        {{ $stage->stageInfo->fullName }} : {{ $stage->time_of_day }} / {{ $stage->weather }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 
     @if ($event->importing)
         @include('dirt-rally.import-in-progress')
