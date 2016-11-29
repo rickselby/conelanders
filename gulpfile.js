741c4fb9c9865e46d8f14d18737e06ea16040a50
Rick Selby <rick@selby-family.co.uk>
2016-11-29 14:29:17 +0000

    Switch to using elixir for front-end management

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var elixir = require('laravel-elixir');
+elixir.config.sourcemaps = false;
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +13,41 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix
+        .sass([
+            /*
+                Currently in here (things that need other assets publishing):
+                    * bootstrap
+                    * font-awesome
+                    * flag-icon
+             */
+            'app.scss',
+        ], 'resources/assets/generated/sass.css')
+        .styles([
+            '../bower/bootstrap-social/bootstrap-social.css',
+            '../bower/tablesorter/dist/css/theme.bootstrap.min.css',
+            '../bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+            '../bower/typeahead.js-bootstrap3.less/typeaheadjs.css',
+            'styles.css',
+            '../generated/sass.css'
+        ])
+        .scripts([
+            '../bower/jquery/dist/jquery.js',
+            '../bower/bootstrap/dist/js/bootstrap.js',
+            '../bower/tablesorter/dist/js/jquery.tablesorter.js',
+            '../bower/tablesorter/dist/js/jquery.tablesorter.widgets.js',
+            '../bower/moment/min/moment.min.js',
+            '../bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+            '../bower/Sortable/jquery.fn.sortable.js',
+            '../bower/typeahead.js/dist/typeahead.jquery.js',
+            '../bower/typeahead.js/dist/bloodhound.js',
+        ])
+        .copy('resources/assets/bower/bootstrap-sass/assets/fonts/bootstrap', 'public/vendor/fonts')
+        .copy('resources/assets/bower/font-awesome/fonts', 'public/vendor/fonts')
+        .copy('resources/assets/bower/flag-icon-css/flags', 'public/vendor/flags')
+        .version([
+            'css/all.css',
+            'js/all.js'
+        ]);
+
 });
